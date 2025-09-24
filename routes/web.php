f05ea7a9fb0e4ce47953b06012842b52cd057d95
rizkyputra0052 <rizkyputra0052@gmail.com>
2025-09-24 23:50:33 +0700

    laravel

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('home');
+});
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/', function () {
+    return view('welcome');
+});
