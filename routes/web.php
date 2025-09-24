e54d81bc609d07c963563be45c255fdd5b90407f
rizkyputra0052 <rizkyputra0052@gmail.com>
2025-09-25 00:00:21 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/about', function () {
+    return view('about');
 });
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/home', function () {
+    return view('home');
 });
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/profile', function () {
+    return view('profile');
 });
